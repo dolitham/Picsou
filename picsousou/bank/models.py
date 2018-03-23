1c6aa6ac23325593c77944b1e9f344dc96a5fecc
Julie Hamadou <dolitham@hotmail.com>
2018-03-23 17:06:58 +0100

    gros commit

--- picsousou/bank/models.py
+++ picsousou/bank/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.utils.timezone import now
 
+RE = "Restos"
+MA = 'Market'
+CH = 'Charges'
+HB = 'HB'
+
+BUDGET_CHOICES = (
+    (RE, 'Restos'),
+    (MA, 'Market'),
+    (CH, 'Charges'),
+    (HB, 'Hors Budget')
+)
 
 class Operation(models.Model):
     name = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     date = models.DateField('Date', default=now())
+    budget = models.CharField(max_length=15, choices=BUDGET_CHOICES, default=HB)
