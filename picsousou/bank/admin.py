6b7f96f732a671333ff1c133e3a1a7f842811a73
Julie Hamadou <dolitham@hotmail.com>
2018-04-05 11:00:03 +0200

    remove migrations and add Budget Model

--- picsousou/bank/admin.py
+++ picsousou/bank/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Operation, Account
+from .models import *
 
-admin.site.register(Operation)
+class OperationAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Operation._meta.get_fields()]
+    #list_display_links = ('name', 'budget')
+
 admin.site.register(Account)
+admin.site.register(PaymentMethod)
+admin.site.register(Operation, OperationAdmin)
+admin.site.register(Person)
+admin.site.register(Budget)
+admin.site.register(Month)
--- picsousou/bank/models.py
+++ picsousou/bank/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 from django.forms import ModelForm
 import django_filters
+import datetime
 
 RE = "Restos"
 MA = 'Market'
@@ -22,16 +23,61 @@ class Month(models.Model):
     total_input = models.DecimalField(max_digits=7, decimal_places=2, default = 0)
     total_output = models.DecimalField(max_digits=7, decimal_places=2, default = 0)
     balance = models.DecimalField(max_digits=7, decimal_places=2, default = 0)
+    #timedelta = last_day - first_day
+    #nb_days = models.IntegerField(default=timedelta.days, editable=False)
+
+    def __str__(self):
+        return self.first_day.strftime('%b %Y')
+
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=50)
+    id_name = models.CharField(max_length=10)
+    balance = models.DecimalField(max_digits=7, decimal_places=2)
+    #person = models.ForeignKey(Person, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class PaymentMethod(models.Model):
+    name = models.CharField(max_length=50)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ' ' + self.account.id_name
+
+
+class Budget(models.Model):
+    name = models.CharField(max_length=20)
+    month = models.ForeignKey(Month, on_delete=models.CASCADE,blank=True, default=1)
+    prevision = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    spent = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.name
 
 
 class Operation(models.Model):
     name = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=7, decimal_places=2)
-    date = models.DateField('Date', default=now())
-    budget = models.CharField(max_length=15, choices=BUDGET_CHOICES, default=HB)
+    date = models.DateField('Date', default=datetime.date.today())
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE, default=1)
+    check = models.BooleanField(default=False)
+    payment = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name + ' : '+str(self.amount)
+
+
+class OperationForm(ModelForm):
+    class Meta:
+        model = Operation
+        fields = ['name', 'amount']
+
 
 
 class OperationFilter(django_filters.FilterSet):
@@ -41,16 +87,7 @@ class OperationFilter(django_filters.FilterSet):
             'name': ['exact', 'icontains',],
             'amount': ['lt', 'gt' ],
             'date': ['lt', 'gt' ],
-            'budget': ['exact', ],
         }
 
 
-class Account(models.Model):
-    name = models.CharField(max_length=50)
-    balance = models.DecimalField(max_digits=7, decimal_places=2)
 
-
-class OperationForm(ModelForm):
-    class Meta:
-        model = Operation
-        fields = ['name', 'amount']
