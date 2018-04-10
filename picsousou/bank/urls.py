5f55455911e3d050f6287aca9f04f64a95300b17
Julie Hamadou <dolitham@hotmail.com>
2018-04-10 20:37:27 +0200

    bank header

--- picsousou/bank/urls.py
+++ picsousou/bank/urls.py
@@ -8,7 +8,6 @@ app_name = 'bank'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('index', index, name='index'),
     path('<int:pk>/', OperationUpdate.as_view(), name='edit_operation'),
     url(r'^search/$', search, name='search'),
     path('add_operation', add_operation, name='add_operation')
